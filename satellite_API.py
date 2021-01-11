5122552794b66c168c409f05512fd5de88af66c7
hulleylm <hulleylm@gmail.com>
2021-01-11 23:57:24 +0000

    static maps API for San Fran example

--- satellite_API.py
+++ satellite_API.py
@@ -1,13 +1,45 @@
 import requests
+import csv
+import pandas as pd
+import matplotlib.path as mpltPath
+
+def checkIfLand(path, lat, lng):
+    if (path.contains_point([lng, lat])):
+        center = str(lat) + "," + str(lng)
+        requestImage(center)
+
+def requestImage(center):
+    url = "https://maps.googleapis.com/maps/api/staticmap?center=" + center + \
+        "&zoom=16&size=640x640&key=" + api_key + "&maptype=satellite&scale=2"
+    filename = AreaID + str(row) + "," + str(col) + ".png"
+    r = requests.get(url)
+    f = open(filename, 'wb')
+    f.write(r.content)
+    f.close()
+    print("writtern to file")
 
 api_key = open("config.txt", "r", encoding="utf-16").read()
+polygon = pd.read_csv("satellite_images/sanFranPoly.csv").to_numpy()
+path = mpltPath.Path(polygon)
 
-zoom = "10"
-center = "40.714%2c%20-73.998"
+# set overall area to be scanned
+AreaID = "SanFran"
+NWlat = 38.1273952
+NWlng = -122.545723
+SElat = 37.6223939
+SElng = -122.3260534
+step = 0.0120359
 
-url = "https://maps.googleapis.com/maps/api/staticmap?center=" + center + "&zoom=" + zoom + "&size=400x400&key=" + api_key
+rows = int(round((NWlat - SElat)/step))
+columns = int(round((abs(NWlng) - abs(SElng))/step))
 
-r = requests.get(url)
-f = open('output.png', 'wb')
-f.write(r.content)
-f.close()
+lat = NWlat
+
+for row in range(rows):
+    lng = NWlng
+    for col in range(columns):
+        checkIfLand(path, lat, lng)
+        lng = lng + step
+        print (str(lat) + "," + str(lng) + " row: " + str(row) + " col: " + str(col))
+    lat = lat - step
+
