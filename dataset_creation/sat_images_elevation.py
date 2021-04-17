cbf2d462deff7ee19d44b2f9a13d60fec3ce4c7c
hulleylm <hulleylm@gmail.com>
2021-04-17 13:35:47 +0100

    updated file path

--- dataset_creation/sat_images_elevation.py
+++ dataset_creation/sat_images_elevation.py
@@ -1,85 +1,86 @@
 import requests
-import csv # to read polygon
-import pandas as pd
-import matplotlib.path as mpltPath
 import math
 
+def latLngToPoint(mapWidth, mapHeight, lat, lng):
 
-def latLngToPoint(mapSize, lat, lng):
-
-    x = (lng + 180) * (mapSize/360)
-    y = ((1 - math.log(math.tan(lat * math.pi / 180) + 1 / math.cos(lat * math.pi / 180)) / math.pi) / 2) * mapSize
+    x = (lng + 180) * (mapWidth/360)
+    y = ((1 - math.log(math.tan(lat * math.pi / 180) + 1 / math.cos(lat * math.pi / 180)) / math.pi) / 2) * mapHeight
 
     return(x, y)
 
-def pointToLatLng(mapSize, x, y):
+def pointToLatLng(mapWidth, mapHeight, x, y):
 
-    lng = x / mapSize * 360 - 180
-    n = math.pi - 2 * math.pi * y / mapSize
+    lng = x / mapWidth * 360 - 180
+
+    n = math.pi - 2 * math.pi * y / mapHeight
     lat = (180 / math.pi * math. atan(0.5 * (math.exp(n) - math.exp(-n))))
 
     return(lat, lng)
 
-def getImageBounds(picSize, mapSize, scale, lat, lng):
+def getImageBounds(mapWidth, mapHeight, xScale, yScale, lat, lng):
 
-    centreX, centreY = latLngToPoint(mapSize, lat, lng)
+    centreX, centreY = latLngToPoint(mapWidth, mapHeight, lat, lng)
 
-    southWestX = centreX - (mapSize/2)/ scale
-    southWestY = centreY + (mapSize/2)/ scale
-    SWlat, SWlng = pointToLatLng(mapSize, southWestX, southWestY)
+    southWestX = centreX - (mapWidth/2)/ xScale
+    southWestY = centreY + (mapHeight/2)/ yScale
+    SWlat, SWlng = pointToLatLng(mapWidth, mapHeight, southWestX, southWestY)
 
-    northEastX = centreX + (mapSize/2)/ scale
-    northEastY = centreY - (mapSize/2)/ scale
-    NElat, NElng = pointToLatLng(mapSize, northEastX, northEastY)
+    northEastX = centreX + (mapWidth/2)/ xScale
+    northEastY = centreY - (mapHeight/2)/ yScale
+    NElat, NElng = pointToLatLng(mapWidth, mapHeight, northEastX, northEastY)
 
     return[SWlat, SWlng, NElat, NElng]
 
-def getLatStep(picSize, mapSize, scale, lat, lng):
+def getLatStep(mapWidth, mapHeight, yScale, lat, lng):
 
-    pointX, pointY = latLngToPoint(mapSize, lat, lng)
+    pointX, pointY = latLngToPoint(mapWidth, mapHeight, lat, lng)
 
-    steppedPointY = pointY - ((mapSize)/ scale)
-    newLat, originalLng = pointToLatLng(mapSize, pointX, steppedPointY)
+    steppedPointY = pointY - ((mapHeight)/ yScale)
+    newLat, originalLng = pointToLatLng(mapWidth, mapHeight, pointX, steppedPointY)
 
     latStep = lat - newLat
 
     return (latStep)
 
-def checkIfLand(path, lat, lng):
+def requestImage(picHeight, picWidth, zoom, scale, maptype, lat, lng, row, col):
 
-    if (path.contains_point([lng, lat])):
-        center = str(lat) + "," + str(lng)
-        return True
-    return False
+    center = str(lat) + "," + str(lng)
+    url = "https://maps.googleapis.com/maps/api/staticmap?center=" + center + "&zoom=" + str(zoom) + "&size=" + str(picWidth) + "x" + str(picHeight) + "&key=" + api_key + "&maptype=" + maptype + "&scale=" + str(scale)
+    filename = "dataset_creation/output/" + AreaID + str(col) + "," + str(row) + ".png"
 
-def requestImage(picSize, zoom, center, row, col):
-
-    url = "https://maps.googleapis.com/maps/api/staticmap?center=" + center + \
-        "&zoom=16&size=" + str(picSize) + "x" + str(picSize) + "&key=" + api_key + "&maptype=satellite&scale=2"
-    filename = AreaID + str(row) + "," + str(col) + ".png"
     r = requests.get(url)
     f = open(filename, 'wb')
     f.write(r.content)
     f.close()
-    print("writtern to file" + filename)
 
-api_key = open("config.txt", "r", encoding="utf-16").read()
+    print("writtern to file: " + filename)
 
 # Bounding box for area to be scanned. AreaID is added to file name.
-AreaID = "SanFran"
-northWestLat = 38.1273952
-northWestLng = -122.545723
-southEastLat = 37.6223939
-southEastLng = -122.3260534
+AreaID = "SanFran15_"
+northWestLat = 37.806716
+northWestLng = -122.477702
+southEastLat = 37.7636132
+southEastLng = -122.4319237
 
-zoom = 16
-picSize = 640
-mapSize = 256 #Google map size, keep at 256
-scale = math.pow(2, zoom) / (picSize/mapSize)
+# Variables for API request (more info in README)
+api_key = open("config.txt", "r", encoding="utf-16").read()
+zoom = 15
+picHeight = 640
+picWidth = 640
+scale = 2
+maptype = "satellite"
+
+# --- do not change variables below this point ---
+
+mapHeight = 256
+mapWidth = 256
+xScale = math.pow(2, zoom) / (picWidth/mapWidth)
+yScale = math.pow(2, zoom) / (picHeight/mapWidth)
 
 startLat = northWestLat
 startLng = northWestLng
-startCorners = getImageBounds(picSize, mapSize, scale, startLat, startLng)
+
+startCorners = getImageBounds(mapWidth, mapHeight, xScale, yScale, startLat, startLng)
 lngStep = startCorners[3] - startCorners[1]
 
 col = 0
@@ -90,10 +91,9 @@ while (lat >= southEastLat):
     row = 0
 
     while lng <= southEastLng:
-        center = str(lat) + "," + str(lng)
-        requestImage(picSize, zoom, center, row, col)
+        requestImage(picHeight, picWidth, zoom, scale, maptype, lat, lng, row, col)
         row = row + 1
         lng = lng + lngStep
 
     col = col - 1
-    lat = lat + getLatStep(picSize, mapSize, scale, lat, lng)
+    lat = lat + getLatStep(mapWidth, mapHeight, yScale, lat, lng)
